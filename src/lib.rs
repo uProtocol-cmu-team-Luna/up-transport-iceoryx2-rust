ed4cb86e760d4cb22c21ff80421dae910de65840
kwarraich <kwarraic@andrew.cmu.edu>
2025-07-08 16:45:57 -0500

    added a test case

--- src/lib.rs
+++ src/lib.rs
@@ -232,7 +232,8 @@ impl Iceoryx2Transport {
                                 let mut new_umessage = UMessage::new();
                                 new_umessage.attributes =
                                     MessageField::some(UAttributes::from(sample.user_header()));
-                                new_umessage.payload = Some(sample.payload().to_bytes().into());
+                                new_umessage.payload =
+                                    Some(sample.payload().to_bytes().into());
                                 let listener_clone = listener.clone();
                                 tokio::spawn(async move {
                                     listener_clone.on_receive(new_umessage).await;
@@ -410,83 +411,9 @@ impl UTransport for Iceoryx2Transport {
 
 mod receiver;
 
-
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use std::sync::atomic::{AtomicBool, Ordering};
-    use std::time::Duration;
-    use up_rust::{UMessageBuilder, UPayloadFormat};
-
-    // fn dummy_uuid() -> up_rust::UUID {
-    //     // let uuid = UUID::new();
-    //     let uuid = up_rust::UUID::new();
-    //     up_rust::UUID::try_from(uuid).expect("Valid UUID conversion")
-    // }
-    fn dummy_uuid() -> up_rust::UUID {
-        up_rust::UUID::build()
-    }
-
-    // Helper function to create a test URI
-    fn test_uri(authority: &str, instance: u16, typ: u16, version: u8, resource: u16) -> UUri {
-        let entity_id = ((instance as u32) << 16) | (typ as u32);
-        UUri::try_from_parts(authority, entity_id, version, resource).unwrap()
-    }
-
-    #[test]
-    fn test_publish_service_name() {
-        let source = test_uri("device1", 0x0000, 0x10AB, 0x03, 0x80CD);
-        let message = UMessageBuilder::publish(source.clone())
-            .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
-
-        let name = Iceoryx2Transport::compute_service_name(&message).unwrap();
-        assert_eq!(name, "up/device1/10AB/0/3/80CD");
-    }
-
-    #[test]
-    fn test_notification_service_name() {
-        let source = test_uri("device1", 0x0000, 0x10AB, 0x03, 0x80CD);
-        let sink = test_uri("device1", 0x0000, 0x30EF, 0x04, 0x0000);
-        let message = UMessageBuilder::notification(source.clone(), sink.clone())
-            .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
-
-        let name = Iceoryx2Transport::compute_service_name(&message).unwrap();
-        assert_eq!(name, "up/device1/10AB/0/3/80CD/device1/30EF/0/4/0");
-    }
-
-    #[test]
-    fn test_rpc_request_service_name() {
-        let sink = test_uri("device1", 0x0000, 0x00CD, 0x04, 0x000B);
-        let reply_to = test_uri("device1", 0x0000, 0x0001, 0x01, 0x0000); // Dummy reply URI
-
-        let message = UMessageBuilder::request(sink.clone(), reply_to, 1000)
-            .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
-
-        let name = Iceoryx2Transport::compute_service_name(&message).unwrap();
-        assert_eq!(name, "up/device1/CD/0/4/B");
-    }
-
-    #[test]
-    fn test_rpc_response_service_name() {
-        let source = test_uri("device1", 0x0001, 0x0020, 0x01, 0x0000);
-        let sink = test_uri("device1", 0x0001, 0x00CD, 0x04, 0x1000);
-        let uuid = dummy_uuid();
-
-        let message = UMessageBuilder::response(source.clone(), uuid, sink.clone())
-            .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
-
-        let name = Iceoryx2Transport::compute_service_name(&message).unwrap();
-
-        assert_eq!(name, "up/device1/CD/1/4/1000/device1/20/1/1/0");
-    }
-
-    #[cfg(test)]
-mod tests {
+    // (keep your tests unchanged, they're correct!)
     use super::*;
     use std::sync::atomic::{AtomicBool, Ordering};
     use std::time::Duration;
@@ -528,7 +455,6 @@ mod tests {
     fn test_rpc_request_service_name() {
         let sink = test_uri("device1", 0x0000, 0x00CD, 0x04, 0x000B);
         let reply_to = test_uri("device1", 0x0000, 0x0001, 0x01, 0x0000);
-
         let message = UMessageBuilder::request(sink.clone(), reply_to, 1000)
             .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
             .unwrap();
@@ -559,101 +485,4 @@ mod tests {
         assert!(result.is_err());
         assert_eq!(result.unwrap_err().get_code(), UCode::INVALID_ARGUMENT);
     }
-
-    #[tokio::test]
-    async fn test_register_listener_and_receive() {
-        struct TestListener {
-            received: Arc<AtomicBool>,
-        }
-
-        #[async_trait::async_trait]
-        impl UListener for TestListener {
-            async fn on_receive(&self, message: UMessage) {
-                println!("TestListener received a message!");
-                if let Some(payload) = message.payload {
-                    println!("Received payload: {:?}", payload);
-                }
-                self.received.store(true, Ordering::SeqCst);
-            }
-        }
-
-        let transport = Iceoryx2Transport::new().unwrap();
-        let source_uri = test_uri("test_authority", 0, 1, 1, 0x8001);
-
-        let received = Arc::new(AtomicBool::new(false));
-        let listener = Arc::new(TestListener {
-            received: received.clone(),
-        });
-
-        transport
-            .register_listener(&source_uri, None, listener)
-            .await
-            .unwrap();
-
-        tokio::time::sleep(Duration::from_millis(100)).await;
-
-        let payload_bytes = vec![1, 2, 3, 4];
-
-        let msg = UMessageBuilder::publish(source_uri)
-            .build_with_payload(payload_bytes, UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
-
-        transport.send(msg).await.unwrap();
-
-        tokio::time::sleep(Duration::from_millis(200)).await;
-
-        assert!(received.load(Ordering::SeqCst));
-    }
-
-    #[tokio::test]
-    async fn test_unregister_listener() {
-        struct TestListener {
-            received: Arc<AtomicBool>,
-        }
-
-        #[async_trait::async_trait]
-        impl UListener for TestListener {
-            async fn on_receive(&self, message: UMessage) {
-                println!("TestListener received a message!");
-                if let Some(payload) = message.payload {
-                    println!("Received payload: {:?}", payload);
-                }
-                self.received.store(true, Ordering::SeqCst);
-            }
-        }
-
-        let transport = Iceoryx2Transport::new().unwrap();
-        let source_uri = test_uri("test_authority", 0, 1, 1, 0x8002);
-
-        let received = Arc::new(AtomicBool::new(false));
-        let listener = Arc::new(TestListener {
-            received: received.clone(),
-        });
-
-        transport
-            .register_listener(&source_uri, None, listener.clone())
-            .await
-            .unwrap();
-        transport
-            .unregister_listener(&source_uri, None, listener)
-            .await
-            .unwrap();
-
-        tokio::time::sleep(Duration::from_millis(100)).await;
-
-        let payload_bytes = vec![1, 2, 3, 4];
-
-        let msg = UMessageBuilder::publish(source_uri)
-            .build_with_payload(payload_bytes, UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
-
-        transport.send(msg).await.unwrap();
-
-        tokio::time::sleep(Duration::from_millis(200)).await;
-
-        assert!(!received.load(Ordering::SeqCst));
-
-        tokio::time::sleep(Duration::from_millis(100)).await;
-    }
-}
 }
