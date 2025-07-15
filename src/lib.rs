9fe7d53a72bb1d715d3e6ece524b8327c5716699
vidishac2004 <144144085+vidishac2004@users.noreply.github.com>
2025-07-15 21:46:14 +0530

    Changed the conversion function so that it takes source and sink URI's as arguments.
    Some things to figure out:
    
    1.Do we need the option type parameter for sink UURI's just for the publish case?
    
    In the serviceName  RESPONSE example- 403AB is truncate to 3AB, do we need to carry out truncation? Becuase from the examples provided to us in the spec, the ue_type is 403AB, but if i enter that in the test, it shows overflow, so do we have to alter our code for those cases?
    
    Do we need to check for the fact that if publish sink is provided, it must be equal to the authority name?

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use up_rust::{UListener, UMessage, UStatus, UTransport, UUri, UCode};
 /// such as the service connection and active listeners.
 pub struct Iceoryx2Transport {}
 
-// The #[async_trait] attribute enables async functions in our trait impl.
+
 #[async_trait]
 impl UTransport for Iceoryx2Transport {
     async fn send(&self, _message: UMessage) -> Result<(), UStatus> {
@@ -48,44 +48,47 @@ impl Iceoryx2Transport {
         format!("{:X}", value)
     }
 
-    fn compute_service_name(message: &UMessage) -> Result<String, UStatus> {
+
+    fn compute_service_name(source: &UUri,sink: Option<&UUri>) -> Result<String, UStatus> {
         let join_segments = |segments: Vec<String>| segments.join("/");
-
-        if message.is_publish() {
-            let source = message.source().ok_or_else(|| {
-                UStatus::fail_with_code(UCode::INVALID_ARGUMENT, "Missing source URI")
-            })?;
-            let segments = Self::encode_uuri_segments(source);
+        
+        // checking for REQUEST: source.resource_id=0 and 1<=sink.resource_id<=0x7FFF
+        if ((source.resource_id==0 )&& !(sink.is_none())){
+            if !((1<=sink.unwrap().resource_id && sink.unwrap().resource_id<=0x7FFF)){
+                UStatus::fail_with_code(UCode::INVALID_ARGUMENT, "Invalid sink URI for RPC request");
+            }
+            let segments = Self::encode_uuri_segments(sink.unwrap());
             Ok(format!("up/{}", join_segments(segments)))
-        } else if message.is_request() {
-            let sink = message.sink().ok_or_else(|| {
-                UStatus::fail_with_code(UCode::INVALID_ARGUMENT, "Missing sink URI")
-            })?;
-            let segments = Self::encode_uuri_segments(sink);
-            Ok(format!("up/{}", join_segments(segments)))
-        } else if message.is_response() || message.is_notification() {
-            let source = message.source().ok_or_else(|| {
-                UStatus::fail_with_code(UCode::INVALID_ARGUMENT, "Missing source URI")
-            })?;
-            let sink = message.sink().ok_or_else(|| {
-                UStatus::fail_with_code(UCode::INVALID_ARGUMENT, "Missing sink URI")
-            })?;
-
-            let source_segments = Self::encode_uuri_segments(source);
-            let sink_segments = Self::encode_uuri_segments(sink);
-            Ok(format!(
+        }
+        // checking for RESPONSE AND NOTIF: sink.resource_id=0 and 1<=source.resource_id<=0xFFFE
+        else if ((!(sink.is_none()))&&(sink.unwrap().resource_id==0)){
+            if (1<=source.resource_id && source.resource_id <=0xFFFE){
+                let source_segments = Self::encode_uuri_segments(source);
+                let sink_segments = Self::encode_uuri_segments(sink.unwrap());
+                Ok(format!(
                 "up/{}/{}",
                 join_segments(source_segments),
                 join_segments(sink_segments)
-            ))
-        } else {
+                ))
+            }
+            else{
+            Err(UStatus::fail_with_code(UCode::INVALID_ARGUMENT, "Invalid sink and source URIs"))}
+        }
+        // checking for PUBLISH: 1 <=source.resource_id<=0x7FFF 
+        else if ((1<=source.resource_id && source.resource_id<=0x7FFF)){
+            let segments = Self::encode_uuri_segments(source);
+            Ok(format!("up/{}", join_segments(segments)))
+        }
+        else{
             Err(UStatus::fail_with_code(
                 UCode::INVALID_ARGUMENT,
                 "Unsupported UMessageType",
             ))
         }
+
     }
-}
+    }
+
 
 #[cfg(test)]
 mod tests {
@@ -103,58 +106,48 @@ mod tests {
 
     #[test]
     fn test_publish_service_name() {
-        let source = test_uri("device1", 0x0000, 0x10AB, 0x03, 0x80CD);
-        let message = UMessageBuilder::publish(source.clone())
-            .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
+        let source = test_uri("device1", 0x0000, 0x10AB, 0x03, 0x7FFF);
+        // let message = UMessageBuilder::publish(source.clone())
+        //     .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
+        //     .unwrap();
 
-        let name = Iceoryx2Transport::compute_service_name(&message).unwrap();
-        assert_eq!(name, "up/device1/10AB/0/3/80CD");
+        let name = Iceoryx2Transport::compute_service_name(&source,None).unwrap();
+        assert_eq!(name, "up/device1/10AB/0/3/7FFF");
     }
 
     #[test]
     fn test_notification_service_name() {
         let source = test_uri("device1", 0x0000, 0x10AB, 0x03, 0x80CD);
         let sink = test_uri("device1", 0x0000, 0x30EF, 0x04, 0x0000);
-        let message = UMessageBuilder::notification(source.clone(), sink.clone())
-            .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
-
-        let name = Iceoryx2Transport::compute_service_name(&message).unwrap();
+        let name = Iceoryx2Transport::compute_service_name(&source,Some(&sink)).unwrap();
         assert_eq!(name, "up/device1/10AB/0/3/80CD/device1/30EF/0/4/0");
     }
 
     #[test]
     fn test_rpc_request_service_name() {
-        let sink = test_uri("device1", 0x0000, 0x00CD, 0x04, 0x000B);
-        let reply_to = test_uri("device1", 0x0000, 0x0001, 0x01, 0x0000);
+        let sink = test_uri("device1", 0x0004, 0x03AB, 0x03, 0x0000);
+        let reply_to = test_uri("device1", 0x0000, 0x00CD, 0x04, 0xB);
 
-        let message = UMessageBuilder::request(sink.clone(), reply_to, 1000)
-            .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
-
-        let name = Iceoryx2Transport::compute_service_name(&message).unwrap();
+        let name = Iceoryx2Transport::compute_service_name(&sink,Some(&reply_to)).unwrap();
         assert_eq!(name, "up/device1/CD/0/4/B");
     }
 
     #[test]
     fn test_rpc_response_service_name() {
-        let source = test_uri("device1", 0x0001, 0x0020, 0x01, 0x0000);
-        let sink = test_uri("device1", 0x0001, 0x00CD, 0x04, 0x1000);
+        let source = test_uri("device1", 0x0000, 0x00CD, 0x04, 0xB);
+        let sink = test_uri("device1", 0x0004, 0x3AB, 0x3, 0x0000);
         let uuid = dummy_uuid();
 
-        let message = UMessageBuilder::response(source.clone(), uuid, sink.clone())
-            .build_with_payload(vec![], UPayloadFormat::UPAYLOAD_FORMAT_RAW)
-            .unwrap();
+        
 
-        let name = Iceoryx2Transport::compute_service_name(&message).unwrap();
-        assert_eq!(name, "up/device1/CD/1/4/1000/device1/20/1/1/0");
+        let name = Iceoryx2Transport::compute_service_name(&source,Some(&sink)).unwrap();
+        assert_eq!(name, "up/device1/CD/0/4/B/device1/3AB/4/3/0");
     }
 
     #[test]
     fn test_missing_uri_error() {
-        let message = UMessage::new();
-        let result = Iceoryx2Transport::compute_service_name(&message);
+        let uuri = UUri::new();
+        let result = Iceoryx2Transport::compute_service_name(&uuri, None);
 
         assert!(result.is_err());
         assert_eq!(result.unwrap_err().get_code(), UCode::INVALID_ARGUMENT);
